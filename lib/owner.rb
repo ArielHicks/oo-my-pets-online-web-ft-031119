1fb4f7c7aab4f86bafdbc02f64be46c27e24f858
Ariel Hicks <hicks.ariel@gmail.com>
2019-03-25 19:15:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,6 +2,12 @@ class Owner
   OWNERS = []
   attr_accessor :name, :pets
   attr_reader :species
+  def initialize(species)
+    @species = species
+    OWNERS << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
 
   def self.reset_all
     OWNERS.clear
